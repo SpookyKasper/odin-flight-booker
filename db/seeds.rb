be88becd917992c71aa485d5d5103eb72a24baf8
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-03-11 19:03:52 +0100

    Add flight model, associations with airports, seed configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,27 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-["NYC", "BER", "LSB", "LDN", "SFA"].each do |airport_code|
-  Airport.find_or_create_by!(code: airport_code)
+Flight.delete_all
+Airport.delete_all
+
+[["NYC", 1], ["BER", 2], ["LSB",3], ["LDN", 4], ["SFA", 5]].each do |airport_code, id|
+  Airport.find_or_create_by!(code: airport_code, id: id)
+end
+
+
+flight_one = [1, 1, 2, DateTime.now + 10.days, 2.5]
+fligh_two = [2, 3, 5, DateTime.now + 20.days, 3.5]
+flight_three = [3, 2, 4, DateTime.now + 30.days, 2.5]
+fligh_four = [4, 4, 5, DateTime.now + 50.days, 2.5]
+
+flights = [flight_one, fligh_two, flight_three, fligh_four]
+
+flights.each do |array|
+  Flight.find_or_create_by!(
+    id: array[0],
+    departure_airport_id: array[1],
+    arrival_airport_id: array[2],
+    start: array[3],
+    duration: array[4]
+  )
 end
