35a1f86b880aa59efada43bf57bfac9cd626064d
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-03-16 14:36:33 +0100

    Refactor a bit of the booking creation logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,18 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:selected_flight])
-    @booking = @flight.bookings.build
+    @booking = Booking.new
     @passenger_num = params[:selected_passenger_num].to_i
     @passenger_num.times { @booking.passengers.build }
   end
 
   def create
-    @flight = Flight.find(params[:booking][:flight])
+    @flight = Flight.find(params[:booking][:flight_id])
     @booking = @flight.bookings.build(booking_params)
 
     if @booking.save
       flash[:notice] = 'Booking completed ;)'
-      redirect_to @flight
+      redirect_to @booking
     else
       flash[:alert] = 'Problem when saving the booking'
       render :new, status: 422
@@ -22,6 +22,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(passengers_attributes: %i[name email])
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 end
